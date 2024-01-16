8a6d813d0013246d3354711c4efb840fe9e2fcff
krzysiek <krzys_7@op.pl>
2024-01-16 20:38:49 +0100

    ref return demo

---
+++ RefReturn/Program.cs
@@ -0,0 +1,18 @@
+﻿using RefReturn;
+
+TestClass obj = new();
+
+// Get reference
+ref int result = ref obj.GetZ();
+Console.WriteLine(result);
+
+result = 33;
+
+// Get value
+int zValue = obj.GetZ();
+Console.WriteLine(zValue);
+
+
+zValue = 44;
+Console.WriteLine(zValue);
+Console.WriteLine(obj.GetZ());
---
+++ RefReturn/TestClass.cs
@@ -0,0 +1,12 @@
+﻿namespace RefReturn
+{
+    public class TestClass
+    {
+        int z = 9;
+
+        public ref int GetZ()
+        {
+            return ref z;
+        }
+    }
+}
