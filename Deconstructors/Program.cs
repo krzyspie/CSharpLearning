e39bf2c193913c946f2b8db3e95cef66802957a2
krzysiek <krzys_7@op.pl>
2024-01-18 21:30:47 +0100

    How to use deconstructor example

--- Deconstructors/Program.cs
+++ Deconstructors/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using Deconstructors;
+
+var customer = new Customer("Tim", "Beam", "myemail@email.com", "WallStreet", "Paris", "12345", "Spain", "321654987");
+
+string firstName;
+string lastName;
+(firstName, lastName) = customer;
+Console.WriteLine($"Customer name '{firstName} {lastName}'.");
+
+(string customerFirstName, string customerLastName, string email) = customer;
+Console.WriteLine($"Customer name '{customerFirstName} {customerLastName}' with email: '{email}'.");
+
+var (_, _, _, address, city, postalCode, country) = customer;
+Console.WriteLine($"Customer name '{firstName} {lastName}', " +
+    $"address: '{address}', city: '{city}', postal code: '{postalCode}', country: '{country}'.");
+
+string phoneNumber;
+(_, _, string customerEmail, _, _, _, _, phoneNumber) = customer;
+Console.WriteLine($"Customer name '{firstName} {lastName}', with contact information: email '{customerEmail}' phoneNumber '{phoneNumber}'.");
